5ed4c737200676db4dafa94211b5314de07a482a
Guan Xiaoyu <guanxy@me.com>
2016-11-23 19:17:55 +0800

    feat(init): Initial commit.

---
+++ app.py
@@ -0,0 +1,109 @@
+# coding: utf-8
+
+import requests
+import string
+import random
+
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import redirect
+from flask import flash
+from flask import url_for
+from flask_sockets import Sockets
+
+from leancloud import Object
+from leancloud import LeanCloudError
+
+
+app = Flask(__name__)
+sockets = Sockets(app)
+app.secret_key = 'veryverysecret'
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def shorten():
+    lurl = request.form['url']
+    if check_url(lurl) is True:
+        surl = gen_short_url(lurl)
+        return render_template('index.html', surl=request.url_root + surl)
+    else:
+        flash(u'Given URL is dead.', 'error')
+        return render_template('index.html')
+
+@app.route('/<surl>')
+def go(surl):
+    long_url = get_long(surl)
+    Visits = Object.extend('Visits')
+    visit = Visits()
+    visit.set('target', long_url)
+    try:
+        visit.set('ip_address', request.headers['x-real-ip'])
+    except KeyError:
+        visit.set('ip_address', request.remote_addr)
+    visit.set('browser', request.user_agent.browser)
+    visit.set('platform', request.user_agent.platform)
+    visit.set('user_agent_string', request.user_agent.string)
+    visit.save()
+    return redirect(get_long(surl).get('long'))
+
+def check_url(url):
+    try:
+        res = requests.head(url)
+    except:
+        return False
+    if res.status_code < 400:
+        return True
+    else:
+        return False
+
+def gen_random_string(size):
+    random_string = ''.join(random.choice(string.ascii_letters + string.digits) for x in range(size))
+    try:
+        shortened_url = Object.extend('Shortened').query.equal_to('short', random_string).first()
+        return gen_random_string(size + 1)
+    except LeanCloudError as e:
+        if e.code == 101:
+            return random_string
+        else:
+            raise e
+
+def get_short(lurl):
+    surl = Object.extend('Shortened').query.equal_to('long', lurl).first()
+    return surl
+
+def get_long(surl):
+    lurl = Object.extend('Shortened').query.equal_to('short', surl).first()
+    return lurl
+
+def gen_short_url(lurl):
+    '''Generates shortened URL from given long url, returns a string as the
+    shortened url's key.
+    '''
+    try:
+        if get_long(lurl) is not None: return lurl
+    except LeanCloudError as e:
+        if e.code == 101:
+            pass
+        else:
+            raise e
+    try:
+        return get_short(lurl).get('short')
+    except LeanCloudError as e:
+        if e.code == 101:
+            pass
+        else:
+            raise e
+    Shortened = Object.extend('Shortened')
+    shortened = Shortened()
+    # Hard coded size. Fix later ( or never )
+    size = 4
+    surl = gen_random_string(size=size)
+    shortened.set('long', lurl)
+    shortened.set('short', surl)
+    shortened.save()
+    return surl
---
+++ cloud.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+from leancloud import Engine
+
+from app import app
+
+
+engine = Engine(app)
+
+
