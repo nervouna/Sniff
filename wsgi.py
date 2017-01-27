5cc850fa930714737d5e3441492bbb5d3a165938
Guan Xiaoyu <guanxy@me.com>
2017-01-27 23:31:14 +0800

    fix: add secret key

--- wsgi.py
+++ wsgi.py
@@ -11,12 +11,13 @@ APP_ID = os.environ['LEANCLOUD_APP_ID']
 APP_KEY = os.environ['LEANCLOUD_APP_KEY']
 MASTER_KEY = os.environ['LEANCLOUD_APP_MASTER_KEY']
 PORT = int(os.environ['LEANCLOUD_APP_PORT'])
+FLASK_SECRET_KEY = os.eviron['FLASK_SECRET_KEY']
 
 leancloud.init(APP_ID, app_key=APP_KEY, master_key=MASTER_KEY)
 # 如果需要使用 master key 权限访问 LeanCLoud 服务，请将这里设置为 True
 leancloud.use_master_key(False)
 
-application = app
+app.secret_key = FLASK_SECRET_KEY
 
 
 if __name__ == '__main__':
